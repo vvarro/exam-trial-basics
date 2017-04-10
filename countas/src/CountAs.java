225f083f0bf476e30c62490f3f8074475bbe0f59
VVarro <virag.varro@gmail.com>
2017-04-10 11:43:56 +0200

    new function: find the lowercase a-s

--- countas/src/CountAs.java
+++ countas/src/CountAs.java
@@ -7,7 +7,7 @@ public class CountAs {
 
   private final static String FILE_NAME = "afile.txt";
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 
     // Create a function that takes a filename as string parameter,
     // counts the occurances of the letter "a", and returns it as a number.
@@ -15,12 +15,21 @@ public class CountAs {
 
     // example: on the input "afile.txt" the function should return 28 - print this result
     // example: on the input "not-a-file" the function should return 0 - print this result
-
+    Counter(FILE_NAME);
   }
 
   public static void Counter(String FILE_NAME) throws IOException {
     Path path = Paths.get(FILE_NAME);
     String text;
     text = String.valueOf(Files.readAllLines(path));
+    char[] charArray = text.toCharArray();
+    int counter = 0;
+    char character = 'a';
+    for (int i = 0; i < charArray.length; i++) {
+      if (character == charArray[i]) {
+        counter += 1;
+      }
+    }
+    System.out.println(counter);
   }
 }
