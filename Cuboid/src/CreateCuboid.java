b88fda6b399d911c023ecd6c73146f0abb35f358
VVarro <virag.varro@gmail.com>
2017-04-10 12:59:05 +0200

    new class: create cuboid, new function: get the surface

---
+++ Cuboid/src/CreateCuboid.java
@@ -0,0 +1,17 @@
+public class CreateCuboid {
+  int a;
+  int b;
+  int c;
+
+  public CreateCuboid(int a, int b, int c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  public int getSurface(){
+    int surface = 0;
+    surface = 2* (a*b + b*c + c*a);
+    return surface;
+  }
+}
---
+++ Cuboid/src/Cuboid.java
@@ -0,0 +1,6 @@
+public class Cuboid {
+  public static void main(String[] args) {
+    CreateCuboid cuboid = new CreateCuboid(20, 30, 40);
+    cuboid.getSurface();
+  }
+}
