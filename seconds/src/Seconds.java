9f5f7bd3836ca8f2b8e494f3b22160604f0c9c58
VVarro <virag.varro@gmail.com>
2017-04-10 10:56:27 +0200

    new function: split the strig and make it to integer

--- seconds/src/Seconds.java
+++ seconds/src/Seconds.java
@@ -4,20 +4,28 @@ import java.util.Scanner;
 
 public class Seconds {
   public static void main(String[] args) {
-    System.out.println("Please give me the numbers of your list, separeted with ");
+    System.out.println("Please give me the numbers of your list");
     Scanner scanner = new Scanner(System.in);
     String input = scanner.nextLine();
     String[] numbers = input.split(", ");
+    ArrayList<Integer> nums = new ArrayList<>();
+    ArrayList<Integer> seconds = new ArrayList<>();
+    for (String i : numbers) {
+      nums.add(Integer.parseInt(i));
+    }
+    for (Integer i : nums) {
+      System.out.println(i);
+    }
+    Seconds(nums);
   }
 
-
   public static void Seconds(List<Integer> nums) {
-    Integer[] nums = new Integer[numbers.size()];
-    nums = numbers.toArray(nums);
-    for (int i = 0; i < nums.length; i++) {
-      if (nums[i] % 2 == 1) {
-        System.out.println(nums[i] + ", ");
+    List<Integer> seconds = new ArrayList<>();
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums.get(i) % 2 == 1) {
+        seconds.add(nums.get(i));
       }
+      System.out.println(seconds.get(i));
     }
   }
 }
