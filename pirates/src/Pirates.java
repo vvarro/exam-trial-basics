4430834901692f14266b8ace88a43c5718cc25e7
VVarro <virag.varro@gmail.com>
2017-04-10 12:12:42 +0200

    new function: print out the list of rich pirates with wooden leg

--- pirates/src/Pirates.java
+++ pirates/src/Pirates.java
@@ -16,23 +16,15 @@ public class Pirates {
 
   public static void main(String... args) {
     ArrayList<Pirate> pirates = new ArrayList<>();
-
-    // Given this list...
-
-    pirates.add(new Pirate("Olaf", false, 12);
-    pirates.add(new Pirate("Uwe", true, 9);
-    pirates.add(new Pirate("Jack", true, 16);
-    pirates.add(new Pirate("Morgan", false, 17);
-    pirates.add(new Pirate("Hook", true, 20);
-    
-
-    // Write a function that takes any list that contains pirates as in the example,
-    // And returns a list of names containing the pirates that
-    // - have wooden leg and
-    // - have more than 15 gold
+    pirates.add(new Pirate("Olaf", false, 12));
+    pirates.add(new Pirate("Uwe", true, 9));
+    pirates.add(new Pirate("Jack", true, 16));
+    pirates.add(new Pirate("Morgan", false, 17));
+    pirates.add(new Pirate("Hook", true, 20));
+    System.out.println(PirateList(pirates).toString());
   }
 
-  public List<Pirate> PirateList(List<Pirate> pirates) {
+  public static List<Pirate> PirateList(List<Pirate> pirates) {
     List<Pirate> richPirate = new ArrayList<>();
     for (Pirate list : pirates) {
       if (list.hasWoodenLeg == true && list.gold > 15) {
