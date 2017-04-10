8602d5d63fd68dac4348d7d0a9044bab98ab85e9
VVarro <virag.varro@gmail.com>
2017-04-10 11:58:47 +0200

    new function: find rich pirates with wooden leg

---
+++ pirates/src/Pirates.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Pirates {
+
+  static class Pirate {
+    String name;
+    boolean hasWoodenLeg;
+    int gold;
+
+    Pirate(String name, boolean hasWoodenLeg, int gold) {
+      this.name = name;
+      this.hasWoodenLeg = hasWoodenLeg;
+      this.gold = gold;
+    }
+  }
+
+  public static void main(String... args) {
+    ArrayList<Pirate> pirates = new ArrayList<>();
+
+    // Given this list...
+
+    pirates.add(new Pirate("Olaf", false, 12);
+    pirates.add(new Pirate("Uwe", true, 9);
+    pirates.add(new Pirate("Jack", true, 16);
+    pirates.add(new Pirate("Morgan", false, 17);
+    pirates.add(new Pirate("Hook", true, 20);
+    
+
+    // Write a function that takes any list that contains pirates as in the example,
+    // And returns a list of names containing the pirates that
+    // - have wooden leg and
+    // - have more than 15 gold
+  }
+
+  public List<Pirate> PirateList(List<Pirate> pirates) {
+    List<Pirate> richPirate = new ArrayList<>();
+    for (Pirate list : pirates) {
+      if (list.hasWoodenLeg == true && list.gold > 15) {
+        richPirate.add(list);
+      }
+    }
+    return richPirate;
+  }
+}
